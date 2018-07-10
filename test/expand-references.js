b2f9ff82a3bd56438b39fdcb2320f59832e9db67
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2018-07-11 00:12:22 +0200

    Adds test macro for expand-references test

--- test/expand-references.js
+++ test/expand-references.js
@@ -4,41 +4,33 @@ const test = require('ava');
 
 const expandReferencess = require('../source/transforms/expand-references');
 
-test('Func component: array literal', t => {
-  const syntaxTree = parse(
-    'const array=[1,2];Component.propTypes={prop:oneOf(array)};'
-  );
+const template = (t, input, expected) => {
+  const syntaxTree = parse(input, { plugins: ['classProperties'] });
   expandReferencess({ syntaxTree, componentName: 'Component' });
 
-  t.is(
-    generate(syntaxTree, { minified: true }).code,
-    'const array=[1,2];Component.propTypes={prop:oneOf([1,2])};'
-  );
-});
+  t.is(generate(syntaxTree, { minified: true }).code, expected);
+};
 
-test('Func component: object keys', t => {
-  const syntaxTree = parse(
-    'const object={a:1,b:2};Component.propTypes={prop:oneOf(Object.keys(object))}'
-  );
-  expandReferencess({ syntaxTree, componentName: 'Component' });
+test(
+  'Func component: array literal',
+  template,
+  'const array=[1,2];Component.propTypes={prop:oneOf(array)};',
+  'const array=[1,2];Component.propTypes={prop:oneOf([1,2])};'
+);
 
-  t.is(
-    generate(syntaxTree, { minified: true }).code,
-    'const object={a:1,b:2};Component.propTypes={prop:oneOf(["a","b"])};'
-  );
-});
+test(
+  'Func component: object keys',
+  template,
+  'const object={a:1,b:2};Component.propTypes={prop:oneOf(Object.keys(object))}',
+  'const object={a:1,b:2};Component.propTypes={prop:oneOf(["a","b"])};'
+);
 
-test('Func component: object values', t => {
-  const syntaxTree = parse(
-    'const object={a:1,b:2};Component.propTypes={prop:oneOf(Object.values(object))}'
-  );
-  expandReferencess({ syntaxTree, componentName: 'Component' });
-
-  t.is(
-    generate(syntaxTree, { minified: true }).code,
-    'const object={a:1,b:2};Component.propTypes={prop:oneOf([1,2])};'
-  );
-});
+test(
+  'Func component: object values',
+  template,
+  'const object={a:1,b:2};Component.propTypes={prop:oneOf(Object.values(object))}',
+  'const object={a:1,b:2};Component.propTypes={prop:oneOf([1,2])};'
+);
 
 test('Func component: missing literal', t => {
   const syntaxTree = parse(
@@ -50,44 +42,26 @@ test('Func component: missing literal', t => {
   });
 });
 
-test('Class component: array literal', t => {
-  const syntaxTree = parse(
-    'const array=[1,2];class Component{static propTypes={prop:oneOf(array)};}',
-    { plugins: ['classProperties'] }
-  );
-  expandReferencess({ syntaxTree, componentName: 'Component' });
+test(
+  'Class component: array literal',
+  template,
+  'const array=[1,2];class Component{static propTypes={prop:oneOf(array)};}',
+  'const array=[1,2];class Component{static propTypes={prop:oneOf([1,2])}}'
+);
 
-  t.is(
-    generate(syntaxTree, { minified: true }).code,
-    'const array=[1,2];class Component{static propTypes={prop:oneOf([1,2])}}'
-  );
-});
+test(
+  'Class component: object keys',
+  template,
+  'const object={a:1,b:2};class Component{static propTypes={prop:oneOf(Object.keys(object))};}',
+  'const object={a:1,b:2};class Component{static propTypes={prop:oneOf(["a","b"])}}'
+);
 
-test('Class component: object keys', t => {
-  const syntaxTree = parse(
-    'const object={a:1,b:2};class Component{static propTypes={prop:oneOf(Object.keys(object))};}',
-    { plugins: ['classProperties'] }
-  );
-  expandReferencess({ syntaxTree, componentName: 'Component' });
-
-  t.is(
-    generate(syntaxTree, { minified: true }).code,
-    'const object={a:1,b:2};class Component{static propTypes={prop:oneOf(["a","b"])}}'
-  );
-});
-
-test('Func component: object values', t => {
-  const syntaxTree = parse(
-    'const object={a:1,b:2};class Component{static propTypes={prop:oneOf(Object.values(object))}}',
-    { plugins: ['classProperties'] }
-  );
-  expandReferencess({ syntaxTree, componentName: 'Component' });
-
-  t.is(
-    generate(syntaxTree, { minified: true }).code,
-    'const object={a:1,b:2};class Component{static propTypes={prop:oneOf([1,2])}}'
-  );
-});
+test(
+  'Func component: object values',
+  template,
+  'const object={a:1,b:2};class Component{static propTypes={prop:oneOf(Object.values(object))}}',
+  'const object={a:1,b:2};class Component{static propTypes={prop:oneOf([1,2])}}'
+);
 
 test('Class component: missing literal', t => {
   const syntaxTree = parse(
