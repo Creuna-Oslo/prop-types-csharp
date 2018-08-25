6f7f2059c0bbc2aca98da3a2ad5ec6b7a9f9d0f8
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2018-08-25 19:08:30 +0200

    Replaces more snapshots with string comparisons

--- test/lib/ast-to-csharp.js
+++ test/lib/ast-to-csharp.js
@@ -2,97 +2,223 @@ const { parse } = require('@babel/parser');
 const test = require('ava');
 
 const ASTToCsharp = require('../../lib/utils/ast-to-csharp');
+const normalize = require('../utils/_normalize-string');
 
 const basicTree = parse(`
   Component = {
     text: string.isRequired,
     texts: arrayOf(string),
-    singleObject: SingleObject,
-    objects: arrayOf(ObjectsItem).isRequired
+    singleObject: Component_SingleObject,
+    objects: arrayOf(Component_ObjectsItem).isRequired
   };
-  SingleObject = {
+  Component_SingleObject = {
     propertyA: string.isRequired
   };
-  ObjectsItem = {
+  Component_ObjectsItem = {
     propertyB: string
   };
 `);
 
+const imports = `using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;`;
+
+const basicClass = `
+public class Component
+{
+  [Required]
+  public string Text { get; set; }
+  public IList<string> Texts { get; set; }
+  public Component_SingleObject SingleObject { get; set; }
+  [Required]
+  public IList<Component_ObjectsItem> Objects { get; set; }
+}
+public class Component_SingleObject
+{
+  [Required]
+  public string PropertyA { get; set; }
+}
+public class Component_ObjectsItem
+{
+  public string PropertyB { get; set; }
+}
+`;
+
 test('Basic tree', t => {
-  t.snapshot(
-    ASTToCsharp({
-      syntaxTree: basicTree
-    })
+  t.is(
+    normalize(ASTToCsharp({ syntaxTree: basicTree })),
+    normalize(imports + basicClass)
   );
 });
 
 test('With namespace', t => {
-  t.snapshot(
-    ASTToCsharp({
-      namespace: 'Something.SomethingElse',
-      syntaxTree: basicTree
-    })
+  t.is(
+    normalize(
+      ASTToCsharp({
+        namespace: 'Something.SomethingElse',
+        syntaxTree: basicTree
+      })
+    ),
+    normalize(
+      `${imports}\n` + `namespace Something.SomethingElse\n{\n${basicClass}\n}`
+    )
   );
 });
 
 test('With different indentation', t => {
-  t.snapshot(
-    ASTToCsharp({
-      syntaxTree: basicTree,
-      indent: 6
-    })
+  const ast = parse(`
+    Component = {
+      a: string
+    };
+  `);
+  const classString =
+    imports +
+    `
+public class Component
+{
+      public string A { get; set; }
+}
+  `;
+
+  t.is(
+    normalize(
+      ASTToCsharp({
+        indent: 6,
+        syntaxTree: ast
+      }),
+      false
+    ),
+    normalize(classString, false)
   );
 });
 
 test('Optional enum', t => {
-  t.snapshot(
-    ASTToCsharp({
-      syntaxTree: parse(`
-      Component = {
-        enum: Enum
-      };
-      Enum = ['value-1', 'value-2'];
-    `)
-    })
+  const code = `
+    Component = {
+      enum: Enum
+    };
+    Enum = ['value-1', 'value-2'];
+  `;
+
+  t.is(
+    normalize(
+      ASTToCsharp({
+        syntaxTree: parse(code)
+      })
+    ),
+    normalize(
+      imports +
+        `
+      public class Component
+      {
+        public Enum Enum { get; set; }
+      }
+      public enum Enum
+      {
+        None = 0,
+        [Display(Name = "value-1")]
+        Value1 = 1,
+        [Display(Name = "value-2")]
+        Value2 = 2,
+      }
+    `
+    )
   );
 });
 
 test('Required enum', t => {
-  t.snapshot(
-    ASTToCsharp({
-      syntaxTree: parse(`
-        Component = {
-          enum: Enum.isRequired
-        };
-        Enum = ['value-1', 'value-2'];
-      `)
-    })
+  const code = `
+    Component = {
+      enum: Enum.isRequired
+    };
+    Enum = ['value-1', 'value-2'];
+  `;
+
+  t.is(
+    normalize(
+      ASTToCsharp({
+        syntaxTree: parse(code)
+      })
+    ),
+    normalize(
+      imports +
+        `
+      public class Component
+      {
+        [Required]
+        public Enum Enum { get; set; }
+      }
+      public enum Enum
+      {
+        [Display(Name = "value-1")]
+        Value1 = 0,
+        [Display(Name = "value-2")]
+        Value2 = 1,
+      }
+    `
+    )
   );
 });
 
 test('Enum with name starting with non-letter', t => {
-  t.snapshot(
-    ASTToCsharp({
-      syntaxTree: parse(`
-        Component = {
-          enum: Enum
-        };
-        Enum = ['-value-1', '.value-2', '#value-3'];
-      `)
-    })
+  const code = `
+    Component = {
+      enum: Enum
+    };
+    Enum = ['-value-1', '.value-2', '#value-3'];
+  `;
+
+  t.is(
+    normalize(
+      ASTToCsharp({
+        syntaxTree: parse(code)
+      })
+    ),
+    normalize(
+      imports +
+        `
+      public class Component
+      {
+        public Enum Enum { get; set; }
+      }
+      public enum Enum
+      {
+        None = 0,
+        [Display(Name = "-value-1")]
+        Value1 = 1,
+        [Display(Name = ".value-2")]
+        Value2 = 2,
+        [Display(Name = "#value-3")]
+        Value3 = 3,
+      }
+    `
+    )
   );
 });
 
 test('With empty definition', t => {
-  t.snapshot(
-    ASTToCsharp({
-      syntaxTree: parse(`
+  t.is(
+    normalize(
+      ASTToCsharp({
+        syntaxTree: parse(`
         Component = {
           property: Property
         };
         Property = {};
       `)
-    })
+      })
+    ),
+    normalize(
+      imports +
+        `
+        public class Component
+        {
+          public Property Property { get; set; }
+        }
+        public class Property
+        {
+        }
+      `
+    )
   );
 });
 
