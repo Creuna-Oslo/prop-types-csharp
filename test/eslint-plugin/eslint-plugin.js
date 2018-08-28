f6415462e1d85573ccb34b52a272dfdb595725c8
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2018-08-28 20:09:39 +0200

    Adds many tests for eslint plugin

--- test/eslint-plugin/eslint-plugin.js
+++ test/eslint-plugin/eslint-plugin.js
@@ -12,8 +12,9 @@ test('Compatibility with proxy plugin path', t => {
 });
 
 const ruleTester = new RuleTester(test, {
+  parser: 'babel-eslint',
   parserOptions: {
-    ecmaVersion: 2015,
+    ecmaVersion: 6,
     ecmaFeatures: { classes: true, jsx: true },
     sourceType: 'module'
   }
@@ -33,52 +34,119 @@ const validCases = [
   // No object literal in propTypes
   'A.propTypes = B.propTypes;',
 
+  // No object literal in propTypes (class component)
+  'class A { static propTypes = B.propTypes; }',
+
   // Invalid 'object' with meta type
   'A.propTypes = { b: PropTypes.object }; A.propTypesMeta = { b: "exclude" };',
+
+  // Invalid 'object' with meta type (class component)
+  'class A { static propTypes = { b: PropTypes.object }; static propTypesMeta = { b: "exclude" };}',
 
   // Invalid required 'object' with meta type
   'A.propTypes = { b: PropTypes.object.isRequired }; A.propTypesMeta = { b: "exclude" };',
 
+  // Invalid required 'object' with meta type (class component)
+  'class A { static propTypes = { b: PropTypes.object.isRequired }; static propTypesMeta = { b: "exclude" }; }',
+
   // Invalid 'array' with meta type
   'A.propTypes = { b: PropTypes.array }; A.propTypesMeta = { b: "exclude" };',
+
+  // Invalid 'array' with meta type (class component)
+  'class A { static propTypes = { b: PropTypes.array }; static propTypesMeta = { b: "exclude" }; }',
 
   // Invalid 'oneOfType' with meta type
   'A.propTypes = { b: PropTypes.oneOfType() }; A.propTypesMeta = { b: "exclude" };',
 
+  // Invalid 'oneOfType' with meta type (class component)
+  'class A { static propTypes = { b: PropTypes.oneOfType() }; static propTypesMeta = { b: "exclude" }; }',
+
   // Invalid function call with meta type
-  ['A.propTypes = { b: someFunc() }; A.propTypesMeta = { b: "exclude" };'],
+  'A.propTypes = { b: someFunc() }; A.propTypesMeta = { b: "exclude" };',
+
+  // Invalid function call with meta type (class component)
+  'class A { static propTypes = { b: someFunc() }; static propTypesMeta = { b: "exclude" }; }',
 
   // Valid meta type Array()
   'A.propTypesMeta = { b: Array(B) };',
 
+  // Valid meta type Array() (class component)
+  'class A { static propTypesMeta = { b: Array(B) }; }',
+
   // Valid meta type 'exclude'
   'A.propTypesMeta = { b: "exclude" };',
+
+  // Valid meta type 'exclude' (class component)
+  'class A { static propTypesMeta = { b: "exclude" }; }',
 
   // Valid meta type 'int'
   'A.propTypesMeta = { b: "int" };',
 
+  // Valid meta type 'int' (class component)
+  'class A { static propTypesMeta = { b: "int" }; }',
+
   // Valid meta type 'float'
-  'A.propTypesMeta = { b: "float" };'
+  'A.propTypesMeta = { b: "float" };',
+
+  // Valid meta type 'float' (class component)
+  'class A { static propTypesMeta = { b: "float" }; }'
 ].map(code => code + footer);
 
 const invalidCases = [
   // PropTypes.object
   ['A.propTypes = { b: PropTypes.object };' + footer, errors.object],
 
+  // PropTypes.object (class component)
+  [
+    'class A { static propTypes = { b: PropTypes.object };}' + footer,
+    errors.object
+  ],
+
   // PropTypes.array
   ['A.propTypes = { b: PropTypes.array };' + footer, errors.array],
+
+  // PropTypes.array (class component)
+  [
+    'class A { static propTypes = { b: PropTypes.array }; }' + footer,
+    errors.array
+  ],
 
   // PropTypes.object.isRequired
   ['A.propTypes = { b: PropTypes.object.isRequired };' + footer, errors.object],
 
+  // PropTypes.object.isRequired (class component)
+  [
+    'class A { static propTypes = { b: PropTypes.object.isRequired }; }' +
+      footer,
+    errors.object
+  ],
+
   // PropTypes.oneOfType
   ['A.propTypes = { b: PropTypes.oneOfType() };' + footer, errors.oneOfType],
+
+  // PropTypes.oneOfType (class component)
+  [
+    'class A { static propTypes = { b: PropTypes.oneOfType() }; }' + footer,
+    errors.oneOfType
+  ],
 
   // Name collision
   ['A.propTypes = { a: PropTypes.string };' + footer, errors.propNameCollision],
 
+  // Name collision (class component)
+  [
+    'class A { static propTypes = { a: PropTypes.string }; }' + footer,
+    errors.propNameCollision
+  ],
+
   // Invalid function call
   ['A.propTypes = { b: someFunc() };' + footer, errors.illegalFunctionCall],
+
+  // Invalid function call (class component)
+  [
+    'class A { static propTypes = { b: someFunc() }; }' + footer,
+    errors.illegalFunctionCall
+  ],
 
   // Missing export
   ['const A = () => {};', errors.noExport],
