a15813a032330ce51655459ea4d8dc2ca7713fde
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2018-10-18 19:52:21 +0200

    WIP on fix of extending other component

--- debug/index.js
+++ debug/index.js
@@ -2,11 +2,15 @@ const generateClass = require('../lib');
 
 const { components } = require('../fixtures/source-code');
 
+const code = `const Component = () => <div />;
+Component.propTypes = AnotherComponent.propTypes;
+export default Component;`;
+
 console.log(
   generateClass({
     baseClass: 'ReactComponent',
     indent: 4,
     namespace: 'Namespace',
-    sourceCode: components.funcComponent
+    sourceCode: code
   }).code
 );
