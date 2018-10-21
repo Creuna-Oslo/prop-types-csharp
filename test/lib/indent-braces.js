6878ff0503c9978eacb69e2d4a968cb25bdd03b0
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2018-10-21 17:55:51 +0200

    Renames and adds test for indent-braces

---
+++ test/lib/indent-braces.js
@@ -0,0 +1,27 @@
+const test = require('ava');
+
+const indentBraces = require('../../lib/stringify/indent-braces');
+
+test('Adds indentation', t => {
+  const string = `
+{
+{
+{
+hello
+}
+}
+}
+`;
+  const expected = `
+{
+    {
+        {
+            hello
+        }
+    }
+}
+`;
+  const indentedString = indentBraces(string, 4);
+
+  t.is(expected, indentedString);
+});
