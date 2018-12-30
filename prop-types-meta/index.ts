baaf37f7e8d6610078c36b3d9cfdc37807e2c80c
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2018-12-30 17:09:52 +0100

    Adds typescript type for propTypesMeta

---
+++ prop-types-meta/index.ts
@@ -0,0 +1,25 @@
+const metaTypes = {
+  exclude: 'exclude',
+  double: 'double',
+  'double?': 'double?',
+  float: 'float',
+  'float?': 'float?',
+  int: 'int',
+  'int?': 'int?'
+};
+
+type TypeLiteral = {
+  [key: string]: string | TypeLiteral;
+};
+
+type TypeLiteralArray<Elements> = {
+  [E in keyof Elements]: PropTypesMeta<Elements[E]>
+};
+
+export type PropTypesMeta<Props> = {
+  [P in keyof Props]?: Props[P] extends TypeLiteral | TypeLiteral[]
+    ? Props[P] extends TypeLiteral[]
+      ? TypeLiteralArray<Props[P]>
+      : PropTypesMeta<Props[P]>
+    : keyof typeof metaTypes | (keyof typeof metaTypes)[]
+};
