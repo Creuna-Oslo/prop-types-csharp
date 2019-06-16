4c9b9bccc4123045da0ada375c618cfdc92fd43f
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2019-06-16 13:16:29 +0200

    Fixes baseClass

--- test/lib/stringify/kotlin.js
+++ test/lib/stringify/kotlin.js
@@ -35,60 +35,70 @@ const basicDefinition = {
 const basicClass = `
 package Component
 
-data class Component(
+open class Component(
   val text: String,
   val numbers: Array<Array<Array<Int>>>? = null,
   val singleObject: Component_SingleObject? = null,
   val objects: Array<Component_Objects>
 )
 
-data class Component_SingleObject(
+class Component_SingleObject(
   val propertyA: Component_SingleObject_PropertyA
 )
 
-data class Component_SingleObject_PropertyA(
+class Component_SingleObject_PropertyA(
   val propertyB: String? = null
 )
 
-data class Component_Objects(
+class Component_Objects(
   val propertyB: String? = null
 )`;
 
-test('Basic propTypes', t => {
+const template = (
+  t,
+  input,
+  expected,
+  options,
+  className = 'Component',
+  removeIndentation
+) => {
   t.is(
-    normalize(basicClass),
-    normalize(stringify(basicDefinition, 'Component'))
+    normalize(expected, removeIndentation),
+    normalize(stringify(input, className, options), removeIndentation)
   );
-});
+};
 
-test('objectOf', t => {
-  const propTypes = {
+test('Basic propTypes', template, basicDefinition, basicClass);
+
+test(
+  'objectOf',
+  template,
+  {
     a: { type: 'objectOf', children: { type: 'string' } },
     b: { type: 'objectOf', children: { type: 'Link' } },
     c: {
       type: 'objectOf',
       children: { type: 'shape', children: { d: { type: 'string' } } }
     }
-  };
-  const expected = `
-package Component
+  },
+  `package Component
 
-import Link.*
+  import Link.*
 
-data class Component(
-  val a: Map<String, String>? = null,
-  val b: Map<String, Link>? = null,
-  val c: Map<String, Component_C>? = null
-)
-data class Component_C(
-  val d: String? = null
-)`;
+  open class Component(
+    val a: Map<String, String>? = null,
+    val b: Map<String, Link>? = null,
+    val c: Map<String, Component_C>? = null
+  )
+  class Component_C(
+    val d: String? = null
+  )`
+);
 
-  t.is(normalize(expected), normalize(stringify(propTypes, 'Component')));
-});
-
-test('Enum', t => {
-  const propTypes = {
+test(
+  'Enum',
+  template,
+  {
     a: {
       type: 'oneOf',
       children: ['value-1', '-value-2', '.value-3', '#value-4']
@@ -102,52 +112,47 @@ test('Enum', t => {
         { key: '#value-4', value: 'D' }
       ]
     }
-  };
-  const expected = `
-package Component
+  },
+  `package Component
 
-data class Component(
-  val a: Component_A? = null,
-  val b: Component_B? = null
-)
+  open class Component(
+    val a: Component_A? = null,
+    val b: Component_B? = null
+  )
 
-enum class Component_A(val stringValue: String) {
-  value1("value-1"),
-  value2("-value-2"),
-  value3(".value-3"),
-  value4("#value-4");
+  enum class Component_A(val stringValue: String) {
+    value1("value-1"),
+    value2("-value-2"),
+    value3(".value-3"),
+    value4("#value-4");
 
-  override fun toString(): String {
-    return stringValue;
+    override fun toString(): String {
+      return stringValue;
+    }
   }
-}
 
-enum class Component_B(val stringValue: String) {
-  value1("A"),
-  value2("B"),
-  value3("C"),
-  value4("D");
+  enum class Component_B(val stringValue: String) {
+    value1("A"),
+    value2("B"),
+    value3("C"),
+    value4("D");
 
-  override fun toString(): String {
-    return stringValue;
-  }
-}`;
+    override fun toString(): String {
+      return stringValue;
+    }
+  }`
+);
 
-  t.is(normalize(expected), normalize(stringify(propTypes, 'Component')));
-});
-
-test('Namespace', t => {
-  const propTypes = { a: { type: 'string' } };
-  const expected = `
-package ViewModels.Component
-data class Component(
-  val a: String? = null
-)`;
-  t.is(
-    normalize(expected),
-    normalize(stringify(propTypes, 'Component', { namespace: 'ViewModels' }))
-  );
-});
+test(
+  'Namespace',
+  template,
+  { a: { type: 'string' } },
+  `package ViewModels.Component
+  open class Component(
+    val a: String? = null
+  )`,
+  { namespace: 'ViewModels' }
+);
 
 test('Extending other component', t => {
   const expected = `
@@ -156,5 +161,5 @@ typealias Component = OtherComponent`;
   t.is(
     normalize(expected),
     normalize(stringify('OtherComponent', 'Component'))
-  );
+);
 });
