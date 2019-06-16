2f384fef03892fb9232e6f70941b789802ee691a
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2019-06-16 11:16:56 +0200

    Adds suppot for different symbols in indent-braces

--- test/lib/stringify/indent-braces.js
+++ test/lib/stringify/indent-braces.js
@@ -26,6 +26,35 @@ hello
   t.is(expected, indentedString);
 });
 
+test('Supports other symbol types', t => {
+  const string = `
+{
+{
+function() {
+return null;
+}
+}
+(
+{
+}
+)
+}`;
+  const expected = `
+{
+  {
+    function() {
+      return null;
+    }
+  }
+  (
+    {
+    }
+  )
+}`;
+  const indentedString = indentBraces(string, 2, ['{', '('], ['}', ')']);
+  t.is(expected, indentedString);
+});
+
 test("Doesn't crash when parameters are missing", t => {
   t.notThrows(() => {
     indentBraces();
