7830655268a3f5c68b4dd6e4be82b84f4ffcac57
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2019-06-19 19:14:34 +0200

    Adds test helpers in webpack plugin test

--- test/webpack-plugin/webpack-plugin.js
+++ test/webpack-plugin/webpack-plugin.js
@@ -9,122 +9,30 @@ const { classes } = require('../../fixtures/javascript/source-code');
 const normalize = require('../utils/_normalize-string');
 const webpackConfig = require('../../fixtures/javascript/webpack.config');
 
-test.cb('Writes C# files to disk', t => {
-  t.plan(3);
-
+const runWebpack = (entry, pluginOptions, callback) => {
   webpack(
-    webpackConfig({ path: tempy.directory() }, { mode: 'production' }),
+    webpackConfig({ entry, path: tempy.directory() }, pluginOptions),
     (error, stats) => {
-      if (error) {
-        throw error;
-      }
-
-      const compilation = stats.toJson();
-
-      if (stats.hasErrors()) {
-        t.fail(compilation.errors);
-        return;
-      }
-
-      const { outputPath } = compilation;
-
-      const CSharpFilePaths = compilation.assets
-        .filter(asset => asset.name.match(/\.cs$/))
-        .map(file => file.name);
-
-      t.is(CSharpFilePaths.length, 2);
-
-      const CSharpFiles = CSharpFilePaths.reduce((accum, filePath) => {
-        const fileName = path.basename(filePath, '.cs');
-        return Object.assign(accum, {
-          [fileName]: fs.readFileSync(path.join(outputPath, filePath), 'utf-8')
-        });
-      }, {});
-
-      t.is(
-        normalize(classes.classComponent),
-        normalize(CSharpFiles.ClassComponent)
-      );
-
-      t.is(
-        normalize(classes.funcComponent),
-        normalize(CSharpFiles.FunctionalComponent)
-      );
-
-      t.end();
+      if (error) throw error; // To see webpack errors when running tests
+      callback(stats.toJson());
     }
   );
-});
+};
 
-test.cb('Adds base class', t => {
-  t.plan(1);
-
-  webpack(
-    webpackConfig(
-      {
-        entry: './fixtures/javascript/app-baseclass.js',
-        path: tempy.directory(),
-        compilerOptions: {
-          namespace: 'Namespace',
-          baseClass: 'BaseClass'
-        }
-      },
-      { mode: 'production' }
-    ),
-    (error, stats) => {
-      if (error) {
-        throw error;
-      }
-
-      const compilation = stats.toJson();
-
-      if (stats.hasErrors()) {
-        t.fail(compilation.errors);
-        return;
-      }
-
-      const { outputPath } = compilation;
-
-      const CSharpFilePaths = compilation.assets
-        .filter(asset => asset.name.match(/\.cs$/))
-        .map(file => file.name);
-
-      const CSharpFiles = CSharpFilePaths.reduce((accum, filePath) => {
-        const fileName = path.basename(filePath, '.cs');
-        return Object.assign(accum, {
-          [fileName]: fs.readFileSync(path.join(outputPath, filePath), 'utf-8')
-        });
-      }, {});
-
-      t.is(
-        normalize(classes.baseClassComponent),
-        normalize(CSharpFiles.BaseClassComponent)
-      );
-
+const runWebpackAndTest = (title, entry, pluginOptions, assertionFn) => {
+  test.cb(title, t => {
+    runWebpack(entry, pluginOptions, compilation => {
+      const { errors } = compilation;
+      if (errors && errors.length > 0) t.fail(compilation.errors);
+      assertionFn(t, compilation);
       t.end();
-    }
-  );
-});
+    });
+  });
+};
 
-const throwsTemplate = (t, entry, expectedErrorMessage) => {
-  t.plan(3);
-
-  webpack(
-    webpackConfig(
-      {
-        entry,
-        path: tempy.directory()
-      },
-      { mode: 'production' }
-    ),
-    (error, stats) => {
-      if (error) {
-        // To see webpack errors when running tests
-        throw error;
-      }
-
-      const compilation = stats.toJson();
-
+const runWebpackAndTestError = (title, entry, expectedErrorMessage) => {
+  test.cb(title, t => {
+    runWebpack(entry, {}, compilation => {
       t.is(1, compilation.errors.length);
       t.is(normalize(expectedErrorMessage), normalize(compilation.errors[0]));
 
@@ -133,29 +41,88 @@ const throwsTemplate = (t, entry, expectedErrorMessage) => {
         .map(file => file.name);
 
       t.is(CSharpFilePaths.length, 0);
-
       t.end();
-    }
-  );
+    });
+  });
 };
 
-const duplicateComponent1Path = path.resolve(
+runWebpackAndTest(
+  'Writes C# files to disk',
+  './fixtures/javascript/app.js',
+  {},
+  (t, compilation) => {
+    const { outputPath } = compilation;
+    const CSharpFilePaths = compilation.assets
+      .filter(asset => asset.name.match(/\.cs$/))
+      .map(file => file.name);
+
+    t.is(CSharpFilePaths.length, 2);
+
+    const CSharpFiles = CSharpFilePaths.reduce((accum, filePath) => {
+      const fileName = path.basename(filePath, '.cs');
+      return Object.assign(accum, {
+        [fileName]: fs.readFileSync(path.join(outputPath, filePath), 'utf-8')
+      });
+    }, {});
+
+    t.is(
+      normalize(classes.classComponent),
+      normalize(CSharpFiles.ClassComponent)
+    );
+
+    t.is(
+      normalize(classes.funcComponent),
+      normalize(CSharpFiles.FunctionalComponent)
+    );
+  }
+);
+
+runWebpackAndTest(
+  'Adds base class',
+  './fixtures/javascript/app-baseclass.js',
+  {
+    compilerOptions: {
+      namespace: 'Namespace',
+      baseClass: 'BaseClass'
+    }
+  },
+  (t, compilation) => {
+    const { outputPath } = compilation;
+
+    const CSharpFilePaths = compilation.assets
+      .filter(asset => asset.name.match(/\.cs$/))
+      .map(file => file.name);
+
+    const CSharpFiles = CSharpFilePaths.reduce((accum, filePath) => {
+      const fileName = path.basename(filePath, '.cs');
+      return Object.assign(accum, {
+        [fileName]: fs.readFileSync(path.join(outputPath, filePath), 'utf-8')
+      });
+    }, {});
+
+    t.is(
+      normalize(classes.baseClassComponent),
+      normalize(CSharpFiles.BaseClassComponent)
+    );
+  }
+);
+
+const duplicate1Path = path.resolve(
   __dirname,
   '../../fixtures/javascript/func-component.jsx'
 );
-const duplicateComponent2Path = path.resolve(
+const duplicate2Path = path.resolve(
   __dirname,
   '../../fixtures/javascript/nested-component/func-component.jsx'
 );
 
-test.cb(
+runWebpackAndTestError(
   'Aborts when duplicate names exist',
-  throwsTemplate,
   './fixtures/javascript/app-duplicate-component.js',
   `C# class generator plugin
 Found duplicate component names in:
-FunctionalComponent (${duplicateComponent1Path})
-FunctionalComponent (${duplicateComponent2Path})`
+FunctionalComponent (${duplicate1Path})
+FunctionalComponent (${duplicate2Path})`
 );
 
 const errorComponentPath = path.resolve(
@@ -163,9 +130,8 @@ const errorComponentPath = path.resolve(
   '../../fixtures/javascript/error-component.jsx'
 );
 
-test.cb(
+runWebpackAndTestError(
   'Aborts when class generation fails',
-  throwsTemplate,
   './fixtures/javascript/app-error-component.js',
   `C# class generator plugin
 ${errorComponentPath}
